3e17cf3ebba2674982e2a8959419f3ed72e83265
Matheo Dorseuil <m.dorseuil@rt-iut.re>
2025-12-15 20:34:00 +0400

    fix: Update allowedHosts configuration in vite.config.js for improved server access

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    allowedHosts: [
+      'sae302.matheodorseuil.com',
+      'localhost',
+      '127.0.0.1'
+    ],
     watch: {
       usePolling: true, // utile si tu es sur un réseau ou VM
     },
